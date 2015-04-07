a6672b2921f3e82a0066f2557eab9db5a7778dd5
Charles Flores <caflores@bayesforecast.com>
2015-04-07 12:54:42 +0200

    Quality Service
    Interface and implementation classes added but to be developed.

---
+++ src/main/java/org/grasia/reptxthank/service/quality/implementation/QualityServiceImpl.java
@@ -0,0 +1,22 @@
+package org.grasia.reptxthank.service.quality.implementation;
+
+import java.util.ArrayList;
+
+import org.grasia.reptxthank.model.Item;
+import org.grasia.reptxthank.service.quality.QualityService;
+
+public class QualityServiceImpl implements QualityService{
+	
+	@Override
+	public long qualityXThank(long itemId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Item> qualityXThank(ArrayList<Item> items) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
